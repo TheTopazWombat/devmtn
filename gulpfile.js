a9d15d94574964ad0d4df02eb0710b76ba7bba92
W. Isaac Leavitt <iceman.ike@gmail.com>
2016-08-03 19:46:30 -0600

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+
+
+gulp.task('js', () => {
+  gulp.src(['./js/app.js', './js/**/*.js'])
+  .pipe(babel({
+    presets: ['es2015']
+  }))
+  .pipe(concat('bundle.js'))
+  .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('css', () => {
+  gulp.src('./styles/**/*.{scss,css}')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('bundle.css'))
+  .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('default', ['js', 'css']);
+
+gulp.watch(['./js/**/*.js'], ['js','concat'])
+gulp.watch(['./styles/**/*.{css,scss}'], ['css'])
